d2c51d4fd81416d4ffcb83415d8a9360cd60d8f6
salazarmendezedwinernesto-design <salazarmendezedwinernesto@gmail.com>
2026-06-29 22:39:14 -0600

    Fix: mover firebase-config/init a public/, arreglar SDK compat, fonts y YouTube API

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -6,7 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const FIREBASE_CONFIG = {
   apiKey: "AIzaSyB6JhtAbjdNhequzwDAKoAV85WgUBuJvow",
   authDomain: "happy-birthday-gaby.firebaseapp.com",
   projectId: "happy-birthday-gaby",
